47b6707db54d3a57705db80573ed5cfd91fb6335
Roman <roman@channel.io>
2022-10-09 20:36:35 +0900

    2-1 버퍼사용하지 않고 연결리스트 내의 중복 요소 제거

--- 용기/practice/src/main/java/LinkedList/NoOverlapping.java
+++ 용기/practice/src/main/java/LinkedList/NoOverlapping.java
@@ -1,31 +1,23 @@
 package LinkedList;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 /**
  * 정렬되어 있지 않은 연결리스트가 주어졌을 때 이 리스트에서 중복되는 워소를 제거하는 코드를 작성하라.
+ * 단, 임시버퍼는 사용하지마라.
  */
 public class NoOverlapping {
 
-  /*
-   * 임시 버퍼 사용하기
-   */
-  public static void play(LinkedList<String> input) {
-    Map<String, Boolean> buffer = new HashMap<>();
-
-    for (int i = 0; ; ) {
-      if (i >= input.size()) {
-        break;
-      }
-      String key = input.get(i);
-      if (!buffer.getOrDefault(key, false)) {
-        buffer.put(input.get(i), true);
-        i++;
-      } else {
-        input.remove(i);
-      }
+  public static void main(String[] args) {
+    BasicLinkedList list = new BasicLinkedList(0);
+    for (int i = 1; i <= 10; i++) {
+      list.append(i);
     }
+    for (int i = 1; i <= 10; i++) {
+      list.append(i);
+    }
+    System.out.printf("Before delete: %s%n", list);
+    list.deleteOverlappedData();
+    System.out.printf("After delete: %s%n", list);
   }
 }
