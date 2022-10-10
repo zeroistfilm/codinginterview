2bce40ab345fb2baae9374201a5937188523ee68
Roman <roman@channel.io>
2022-10-11 00:58:55 +0900

    3-5 스택 정렬

--- 용기/practice/src/main/java/StackAndQueue/BasicStack.java
+++ 용기/practice/src/main/java/StackAndQueue/BasicStack.java
@@ -1,10 +1,16 @@
 package StackAndQueue;
 
 import java.util.EmptyStackException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class BasicStack<T> {
 
   private StackNode<T> top;
+
+  public BasicStack(T init) {
+    this.top = new StackNode<>(init);
+  }
 
   public T pop() {
     if (top == null) throw new EmptyStackException();
@@ -28,12 +34,28 @@ public class BasicStack<T> {
     return top == null;
   }
 
+  @Override
+  public String toString() {
+    List<String> list = new LinkedList<>();
+    StackNode<T> now = this.top;
+    while (now != null) {
+      list.add(now.toString());
+      now = now.next;
+    }
+    return String.join(", ", list);
+  }
+
   private static class StackNode<T> {
     private T data;
     private StackNode<T> next;
 
     public StackNode(T data) {
       this.data = data;
+    }
+
+    @Override
+    public String toString() {
+      return data.toString();
     }
   }
 
