1e2c09c6473eaa74eb7cbb9117375093ed688c02
YD <zeroistfilm@naver.com>
2022-10-11 13:00:16 +0900

    3.5

---
+++ 영동/스택큐/3.5 스택정렬.py
@@ -0,0 +1,30 @@
+# 스택의 가장 작은 값이 위로 오도록 스택을 정렬
+from stack import Stack
+
+
+class Stack3(Stack):
+    def __init__(self):
+        super().__init__()
+        self.tmpStack = Stack()
+        self.tmpValue = None
+
+    def sort(self):
+        while super().isEmpty():
+            data = super().pop()
+            if self.tmpValue == None:
+                self.tmpValue = data
+                continue
+
+            if data <= self.tmpValue:
+               self.tmpStack.push(self.tmpValue)
+               self.tmpValue = data
+
+        print(super().peak())
+
+s = Stack3()
+s.push(10)
+s.push(8)
+s.push(7)
+s.push(11)
+s.push(3)
+s.sort()
