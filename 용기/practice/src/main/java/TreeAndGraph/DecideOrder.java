e0801be728be0ba491c640e6d0cc3669755d6a35
Roman <roman@channel.io>
2022-10-18 05:00:54 +0900

    4-7 설명 주석 추가
    (cherry picked from commit 1d2c77256383d24d0174f87f37c557578a460a66)

--- 용기/practice/src/main/java/TreeAndGraph/DecideOrder.java
+++ 용기/practice/src/main/java/TreeAndGraph/DecideOrder.java
@@ -8,6 +8,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * 순서 정하기 : 프로젝트의 리스트와 프로젝트들 간의 종속 관계(즉, 프로젝트 쌍이 리스트로 주어지면 각 프로젝트 쌍에서 두 번째 프로젝트가 첫 번재 프로젝트에 조속되어 있다는
+ * 뜻)가 주어졌을 대, 프로젝트를 수행해 나가느 순서를 찾으라. 유효한 순서가 존재하지 않으면 에러를 반환한다.
+ */
 public class DecideOrder {
 
   public static void main(String[] args) {
@@ -21,6 +25,82 @@ public class DecideOrder {
         .priorityOrder()
         .toString();
     System.out.println(result);
+  }
+
+  public static class NodeMap {
+
+    // 프로젝트명에 해당하는 Node를 나타낸 맵.
+    private final Map<String, Node> nodesMap;
+    // 해당 노드(프로젝트)가 얼마나 많은 의존성을 가지고 있는가를 나타낸다.
+    private final Map<Node, Integer> dependencyCountMap;
+
+    public NodeMap(Collection<String> projects) {
+      this.nodesMap = new HashMap<>();
+      this.dependencyCountMap = new HashMap<>();
+      for (String project : projects) {
+        Node newNode = new Node(project);
+        nodesMap.put(project, newNode);
+        dependencyCountMap.put(newNode, 0);
+      }
+    }
+
+    // 종속관계 (from, to)는 from 프로젝트가 to 프로젝트보다 우선 수행되어야한다는 것을 말한다.
+    // from Node의 edges에 to Node를 추가한다.
+    public NodeMap applyDependency(String from, String to) {
+      Node fromNode = nodesMap.get(from);
+      fromNode.addEdge(nodesMap.get(to));
+      nodesMap.put(from, fromNode);
+      return this;
+    }
+
+    /*
+     * 1. 모든 노드를 순회한다.
+     * 2. 각 노드에서 DFS로 갈 수 있는 모든 노드를 순회한다. 어떤 노드를 방문하면 방문한 노드에 count를 올린다.
+     * 즉, 특정 프로젝트는 해당 노드의 count만큼 의존하고 있는 프로젝트가 있다.
+     */
+    public NodeMap checkDependency() {
+      for (Node node : nodesMap.values()) {
+        Set<Node> footage = new HashSet<>();
+        for (Node edge : node.edges) {
+          dfs(edge, footage);
+        }
+      }
+      return this;
+    }
+
+    // count 오름차순으로 프로젝트를 나열한다. count가 적을수록 타 프로젝트에 덜 의존적인 프로젝트다.
+    public List<Project> priorityOrder() {
+      return dependencyCountMap.entrySet()
+          .stream()
+          .map(entry -> new Project(entry.getKey(), entry.getValue()))
+          .sorted()
+          .toList();
+    }
+
+    // 특정 노드를 방문하면 map에서 해당 노드의 카운트를 올린다. 그리고 edges를 방문한다.
+    private void dfs(Node to, Set<Node> footage) {
+      if (footage.contains(to)) {
+        return;
+      }
+      dependencyCountMap.compute(to, (key, value) -> value == null ? 0 : ++value);
+      footage.add(to);
+      for (Node edge : to.edges) {
+        dfs(edge, footage);
+      }
+    }
+  }
+
+  public record Project(Node node, Integer priority) implements Comparable<Project> {
+
+    @Override
+    public int compareTo(Project o) {
+      return this.priority.compareTo(o.priority);
+    }
+
+    @Override
+    public String toString() {
+      return node.toString();
+    }
   }
 
   private static class Node {
@@ -41,6 +121,7 @@ public class DecideOrder {
     public int hashCode() {
       return data.hashCode();
     }
+
     @Override
     public boolean equals(Object obj) {
       if (obj == null) {
@@ -55,71 +136,6 @@ public class DecideOrder {
     @Override
     public String toString() {
       return data;
-    }
-  }
-
-  public static class NodeMap {
-
-    private final Map<String, Node> nodesMap;
-    private final Map<Node, Integer> dependencies;
-
-    public NodeMap(Collection<String> projects) {
-      this.nodesMap = new HashMap<>();
-      this.dependencies = new HashMap<>();
-      for (String project : projects) {
-        Node newNode = new Node(project);
-        nodesMap.put(project, newNode);
-        dependencies.put(newNode, 0);
-      }
-    }
-
-    public NodeMap applyDependency(String from, String to) {
-      Node sourceNode = nodesMap.get(from);
-      sourceNode.addEdge(nodesMap.get(to));
-      nodesMap.put(from, sourceNode);
-      return this;
-    }
-
-    public NodeMap checkDependency() {
-      for (Node node : nodesMap.values()) {
-        Set<Node> footage = new HashSet<>();
-        for (Node edge : node.edges) {
-          dfs(edge, footage);
-        }
-      }
-      return this;
-    }
-
-    public List<Project> priorityOrder() {
-      return dependencies.entrySet()
-          .stream()
-          .map(entry -> new Project(entry.getKey(), entry.getValue()))
-          .sorted()
-          .toList();
-    }
-
-    private void dfs(Node to, Set<Node> footage) {
-      if (footage.contains(to)) {
-        return;
-      }
-      dependencies.compute(to, (key, value) -> value == null ? 0 : ++value);
-      footage.add(to);
-      for (Node edge : to.edges) {
-        dfs(edge, footage);
-      }
-    }
-  }
-
-  public record Project(Node node, Integer priority) implements Comparable<Project> {
-
-    @Override
-    public int compareTo(Project o) {
-      return this.priority.compareTo(o.priority);
-    }
-
-    @Override
-    public String toString() {
-      return node.toString();
     }
   }
 }
