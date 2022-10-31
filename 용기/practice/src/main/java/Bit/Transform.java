e065f48326c8ce339819c3a74c0908f7ddb8bcb5
Roman <roman@channel.io>
2022-11-01 00:03:21 +0900

    음수 적용

--- 용기/practice/src/main/java/Bit/Transform.java
+++ 용기/practice/src/main/java/Bit/Transform.java
@@ -5,8 +5,15 @@ public class Transform {
   public static void main(String[] args) {
     int a = 29;
     int b = 15;
+    boolean minus = false;
+
+    if (a * b < 0) {
+      a = Math.abs(a);
+      b = Math.abs(b);
+    }
 
     int mask = a ^ b;
-    System.out.println(Integer.bitCount(mask));
+    int bitCount = Integer.bitCount(mask);
+    System.out.println(minus ? bitCount + 1 : bitCount);
   }
 }
